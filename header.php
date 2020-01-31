30a84272af1b1c6b47f9bd3d8bbcda7b09b0c11e
BrBoot <dulexandre@yahoo.com.br>
2020-01-31 00:34:44 -0300

    Coisas básicas...

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <html lang="pt-br">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Cursos On-line</title>
+
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <img src="" title="Logo" alt="Logo">
+            <div id="menu"></div>
+        </div>    
+    </header>
+
+        <div id="conteudo" class="container">
